cec7ea65f3c570071d94df3568fb30689da6bc42
cdfritz7 <cdfritz7@gmail.com>
2021-02-24 16:50:49 -0600

    small change to make top image sticky

--- js/main.js
+++ js/main.js
@@ -8,3 +8,26 @@ function toggleFunction() {
       x.style.maxHeight = "0px";
     }
 }
+
+function moveScroller() {
+    var $anchor = $("#home");
+    var $scroller = $('#about');
+
+    var move = function() {
+        var st = $(window).scrollTop();
+        var ot = $anchor.offset().top;
+        if(st > ot) {
+            $scroller.css({
+                position: "fixed",
+                top: "0px"
+            });
+        } else {
+            $scroller.css({
+                position: "relative",
+                top: ""
+            });
+        }
+    };
+    $(window).scroll(move);
+    move();
+}
